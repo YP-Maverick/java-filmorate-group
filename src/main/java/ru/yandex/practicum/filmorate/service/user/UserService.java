74b0d9de85f78c05dd272db0384fa2a6ddc0dea6
Лилия <liliahainak@yandex.ru>
2024-03-22 21:31:27 +0500

    fix: добавить NotFoundException.java и deleteFriend() в UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
@@ -15,19 +16,17 @@ public class UserService {
     UserStorage userStorage;
     private final Map<Long, Set<Long>> friends = new HashMap<>();
 
-    private boolean isFriend(Long userId, Long friendId) {
-        Set<Long> userFriends = friends.get(userId);
-        return userFriends.contains(friendId);
+    private void checkId(Long userId, Long otherId) {
+        if ((!userStorage.contains(userId)) || (!userStorage.contains(otherId))) {
+            log.error("Указан id несуществующего пользователя.");
+            throw new ValidationException("Пользователя с таким id не существует.");
+        }
     }
 
     public void addFriend(Long userId, Long friendId) throws ValidationException {
-        if ((!userStorage.contains(userId)) || (!userStorage.contains(friendId))) {
-            log.error("Указан id несуществующего пользователя. Запрос: addFriend.");
-            throw new ValidationException("Пользователя с таким id не существует.");
-        }
+        checkId(userId, friendId);
 
         Set<Long> userFriends;
-
         if (friends.containsKey(userId)) {
             userFriends = friends.get(userId);
             userFriends.add(friendId);
@@ -48,8 +47,22 @@ public class UserService {
         friends.put(friendId, friendFriends);
     }
 
-    public User deleteFriend() {
-        return null;
+    public User deleteFriend(Long userId, Long friendId) throws ValidationException, NotFoundException {
+        checkId(userId, friendId);
+        if (friends.containsKey(userId)) {
+            Set<Long> userFriends = friends.get(userId);
+            if (!userFriends.remove(friendId)) {
+                log.error("Запрос удаления из друзей пользователя по id, которого нет в списке друзей.");
+                throw new NotFoundException("Этого пользователя нет в списке друзей.");
+            } else {
+                Set<Long> friendFriends = friends.get(friendId);
+                friendFriends.remove(userId);
+                return userStorage.getUserById(friendId);
+            }
+        } else {
+            log.error("Запрос удаления из друзей от пользователя, у которого нет друзей.");
+            throw new ValidationException("Этого пользователя нет в списке друзей.");
+        }
     }
 
     public List<User> getAllFriends() {
