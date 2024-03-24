596430fc72cd53eff6a58724968e706f477dddf8
Лилия <liliahainak@yandex.ru>
2024-03-24 21:33:03 +0500

    style: убрать пробелы в UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,9 +5,9 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
-    User create( User user);
+    User create(User user);
 
-    User update( User user);
+    User update(User user);
 
     User delete(Long id);
 
