e15f0594614978a31f6721137f25cc29666d06eb
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-04-29 16:56:39 +0300

    feat: Review модель

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.model;
+
+
+import lombok.Builder;
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Value
+@Builder
+public class Review {
+
+    Long id; // Id отзыва
+
+    @NotBlank(message = "Текст отзыва = NULL")
+    String content;
+
+    @NotNull(message = "Не указан тип отзыва, isPositive = NULL")
+    boolean isPositive; // Положительный/Отрицательный отзыв
+
+    @NotNull(message = "UserId не может быть равен NULL")
+    @Positive
+    Long userId; // Id автора отзыва
+
+    @NotNull(message = "FilmId не может быть равен NULL")
+    @Positive(message = "FilmId не может быть меньше 0")
+    Long filmId; // Id оцениваемого фильма
+
+    // Рейтинг полезности:
+    // При лайке +1 ; При дизлайке -1
+    @NotNull(message = "Useful = null")
+    Integer useful;
+}
