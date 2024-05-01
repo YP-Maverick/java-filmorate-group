8cfd8c536e06254e195b1f27ba134be19ce402ff
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-05-01 12:30:14 +0300

    fix: Поле "isPositive" в json не становится "positive"

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,37 +1,39 @@
 package ru.yandex.practicum.filmorate.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
 import lombok.Builder;
 import lombok.Value;
 import lombok.With;
 
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Value
 @Builder
 public class Review {
 
     @With
+    @JsonProperty("reviewId")
     Long id; // Id отзыва
 
     @NotBlank(message = "Текст отзыва = NULL")
     String content;
 
     @NotNull(message = "Не указан тип отзыва, isPositive = NULL")
-    boolean isPositive; // Положительный/Отрицательный отзыв
+    @SerializedName("isPositive")
+    @JsonProperty("isPositive")
+    Boolean isPositive; // Положительный / Отрицательный отзыв
 
     @NotNull(message = "UserId не может быть равен NULL")
-    @Positive
     Long userId; // Id автора отзыва
 
     @NotNull(message = "FilmId не может быть равен NULL")
-    @Positive(message = "FilmId не может быть меньше 0")
     Long filmId; // Id оцениваемого фильма
 
     // Рейтинг полезности:
     // При лайке +1 ; При дизлайке -1
-    @NotNull(message = "Useful = null")
     Integer useful;
 }
