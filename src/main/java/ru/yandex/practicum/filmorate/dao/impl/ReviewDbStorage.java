3bfca22f0b9302b1dd46183f2ad36285e1a4aaca
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-05-01 12:37:34 +0300

    feat: Ключевые изменения логики репозитория - При создании отзыва useful всегда 0 - При обновлении отзыва можно изменить только content и is_positive - Добавлен метод ReviewDbStorage.getAllReviews - Добавлен метод ReviewDbStorage.getReviewsByFilmId - Добавлен метод deleteReview - Добавлена проверка на повторный лайк/дизлайк

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/ReviewDbStorage.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings("checkstyle:Regexp")
 @AllArgsConstructor
 @Repository
 @Primary
@@ -31,6 +30,9 @@ public class ReviewDbStorage implements ReviewStorage {
     public boolean contains(Long reviewId) {
 
         log.debug("Запрос проверить наличие в БД отзыв с id {}.", reviewId);
+        if (reviewId < 0) {
+            return false;
+        }
 
         String sql = "SELECT COUNT(*) FROM reviews WHERE id = ?";
         int count = jdbcTemplate.queryForObject(sql, Integer.class, reviewId);
@@ -49,13 +51,15 @@ public class ReviewDbStorage implements ReviewStorage {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("id",  review.getId());
         parameters.put("content",  review.getContent());
-        parameters.put("is_positive",  review.isPositive());
+        parameters.put("is_positive",  review.getIsPositive());
         parameters.put("user_id",  review.getUserId());
         parameters.put("film_id",  review.getFilmId());
-        parameters.put("useful0", review.getUseful());
+        parameters.put("useful", 0);
 
         Long userId = simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
-        return review.withId(userId);
+        Review review1 = review.withId(userId);
+        log.warn(review1.toString());
+        return review1;
     }
 
     @Override
@@ -65,32 +69,34 @@ public class ReviewDbStorage implements ReviewStorage {
 
         String sql = "UPDATE reviews " +
                      "SET content = ?," +
-                     " is_positive = ?," +
-                     " user_id = ?, " +
-                     "film_id = ?, " +
-                     "useful = ? " +
+                     " is_positive = ?" +
                      "WHERE id = ?";
 
         int row = jdbcTemplate.update(sql,
                 review.getContent(),
-                review.isPositive(),
-                review.getUserId(),
-                review.getFilmId(),
-                review.getUseful(),
+                review.getIsPositive(),
                 review.getId());
 
         if (row != 1) {
-            log.error("Запрос обновить несуществующего пользователя с id {}.", review.getId());
-            throw new NotFoundException(String.format("Пользователя с id %d не существует.", review.getId()));
-        } else return review;
+            log.error("Запрос обновить несуществующий отзыв с id {}.", review.getId());
+            throw new NotFoundException(String.format("Отзыва с id %d не существует.", review.getId()));
+        } else return getReview(review.getId());
+    }
+
+    @Override
+    public void deleteReview(Long reviewId) {
+        log.debug("Получен запрос удалить отзыв с id {}.", reviewId);
+
+        String sql = "DELETE FROM reviews WHERE id = ?";
+        jdbcTemplate.update(sql, reviewId);
     }
 
     @Override
     public Review getReview(Long reviewId) {
-        log.debug("Запрос получить пользователя с id {}.", reviewId);
+        log.debug("Запрос получить отзыв с id {}.", reviewId);
 
-        String sql = "SELECT * FROM users WHERE id = ?";
-        List<Review> reviews = jdbcTemplate.query(sql, mapper::maleReview, reviewId);
+        String sql = "SELECT * FROM reviews WHERE id = ?";
+        List<Review> reviews = jdbcTemplate.query(sql, mapper::makeReview, reviewId);
 
         return reviews.stream()
                       .findFirst()
@@ -98,33 +104,60 @@ public class ReviewDbStorage implements ReviewStorage {
     }
 
     @Override
+    public List<Review> getAllReviews() {
+        log.debug("Запрос получить все отзывы.");
+
+        String sql = "SELECT * " +
+                     "FROM reviews " +
+                     "ORDER BY useful DESC ";
+
+        return jdbcTemplate.query(sql, mapper::makeReview);
+    }
+
+    @Override
+    public List<Review> getReviewsByFilmId(Long filmId, int count) {
+        log.debug("Запрос получить отзывы по id фильма.");
+
+        String sql = "SELECT * " +
+                     "FROM reviews " +
+                     "WHERE film_id = ? " +
+                     "ORDER BY useful DESC " +
+                     "LIMIT ?";
+
+        return jdbcTemplate.query(sql, mapper::makeReview, filmId, count);
+      }
+
+    @Override
     public void addLike(Long reviewId, Long userId) {
         log.debug("Запрос пользователя с id {} добавить лайк отзыву с id {}.",
                 userId, reviewId);
 
-        String sql = "INSERT INTO review_likes (review_id, user_id) VALUES (?, ?)";
+        String insertLikeSql = "INSERT INTO review_likes (review_id, user_id) VALUES (?, ?)";
+        String updateUsefulSql = "UPDATE reviews SET useful = useful + 1 WHERE id = ?";
+
         try {
-            jdbcTemplate.update(sql, reviewId, userId);
+            jdbcTemplate.update(insertLikeSql, reviewId, userId);
+            jdbcTemplate.update(updateUsefulSql, reviewId);
+
         } catch (DataAccessException e) {
             log.error("Запрос пользователя ({}) повторно поставить лайк отзыву ({}).", userId, reviewId);
             throw new LikeException(String.format("Пользователь %d уже поставил лайк отзыву %d.",
                     userId, reviewId));
         }
-        String reviewLikeSql = "UPDATE reviews SET useful = useful + 1 WHERE id = ?";
-        jdbcTemplate.update(reviewLikeSql, reviewId);
     }
 
     @Override
     public void deleteLike(Long reviewId, Long userId) {
-
         log.debug("Запрос пользователя с id {} удалить лайк у отзыва с id {}.", userId, reviewId);
 
-        String sql = "DELETE FROM review_likes WHERE review_id = ? AND user_id = ?";
+        String deleteLikeSql = "DELETE FROM review_likes WHERE review_id = ? AND user_id = ?";
+        String updateUsefulSql = "UPDATE reviews SET useful = useful - 1 WHERE id = ?";
 
-        int row = jdbcTemplate.update(sql, reviewId, userId);
-        if (row == 1) {
-            String reviewDislikeSql = "UPDATE reviews SET useful = useful - 1 WHERE id = ?";
-            jdbcTemplate.update(reviewDislikeSql, reviewId);
+        try {
+            jdbcTemplate.update(deleteLikeSql, userId);
+            jdbcTemplate.update(updateUsefulSql, reviewId);
+        } catch (DataAccessException e) {
+            log.error("Ошибка при удалении лайка пользователя ({}) у отзыва ({}).", userId, reviewId);
         }
     }
 
@@ -133,28 +166,33 @@ public class ReviewDbStorage implements ReviewStorage {
         log.debug("Запрос пользователя с id {} добавить дизлайк отзыву с id {}.",
                 userId, reviewId);
 
-        String sql = "INSERT INTO review_dislikes (review_id, user_id) VALUES (?, ?)";
+        String insertDislikeSql = "INSERT INTO review_dislikes (review_id, user_id) VALUES (?, ?)";
+        String updateUsefulSql = "UPDATE reviews SET useful = useful - 1 WHERE id = ?";
+
         try {
-            jdbcTemplate.update(sql, reviewId, userId);
+            jdbcTemplate.update(insertDislikeSql, reviewId, userId);
+            jdbcTemplate.update(updateUsefulSql, reviewId);
         } catch (DataAccessException e) {
             log.error("Запрос пользователя ({}) повторно поставить дизлайк отзыву ({}).", userId, reviewId);
             throw new LikeException(String.format("Пользователь %d уже поставил дизлайк отзыву %d.",
                     userId, reviewId));
         }
-        String reviewDislikeSql = "UPDATE reviews SET useful = useful - 1 WHERE id = ?";
-        jdbcTemplate.update(reviewDislikeSql, reviewId);
     }
 
     @Override
     public void deleteDislike(Long reviewId, Long userId) {
         log.debug("Запрос пользователя с id {} удалить дизлайк у отзыва с id {}.", userId, reviewId);
 
-        String sql = "DELETE FROM review_dislikes WHERE review_id = ? AND user_id = ?";
+        String deleteDislikeSql = "DELETE FROM review_dislikes WHERE review_id = ? AND user_id = ?";
+        String updateUsefulSql = "UPDATE reviews SET useful = useful + 1 WHERE id = ?";
 
-        int row = jdbcTemplate.update(sql, reviewId, userId);
-        if (row == 1) {
-            String reviewDislikeSql = "UPDATE reviews SET useful = useful + 1 WHERE id = ?";
-            jdbcTemplate.update(reviewDislikeSql, reviewId);
+        try {
+            jdbcTemplate.update(deleteDislikeSql, reviewId);
+            jdbcTemplate.update(updateUsefulSql, reviewId);
+        } catch (DataAccessException e) {
+            log.error("Ошибка при удалении дизлайка пользователя ({}) у отзыва ({}).", userId, reviewId);
+            throw new LikeException(String.format("Пользователь %d уже поставил дизлайк отзыву %d.",
+                    userId, reviewId));
         }
     }
 }
